ad0027a2d303026c978b2e94d3a3b2d583ef9326
2381049 <2381049@unai.edu>
2024-09-15 21:41:20 +0700

    Handson 3

--- handson2/MenampilkanBilanganGenap.java
+++ handson2/MenampilkanBilanganGenap.java
@@ -1,5 +1,14 @@
 package handson2;
 
 public class MenampilkanBilanganGenap {
-    String  angka[]
+    public static void main(String[] args) {
+        int max = 20
+                for (int i = 2; i <= max; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }
+
+
+                }
+    }
 }
